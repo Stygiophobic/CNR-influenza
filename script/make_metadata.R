9fc9befc9a4b3cbbca198d73c4d29dc81d643c05
Hadrien.Regue <hadrien.regue@univ-lyon1.fr>
2020-01-27 14:15:46 +0100

    Ajout script R

---
+++ script/make_metadata.R
@@ -0,0 +1,40 @@
+argv <- commandArgs(TRUE)
+
+raw_metadata<-read.csv2(as.character(argv[1]))
+H1N1_S4_output<-as.character(argv[2])
+H1N1_S6_output<-as.character(argv[3])
+H3N2_S4_output<-as.character(argv[4])
+H3N2_S6_output<-as.character(argv[5])
+B_S4_output<-as.character(argv[6])
+B_S6_output<-as.character(argv[7])
+
+cleaned_metadata<-subset(raw_metadata,select=c("Seq_Id..HA.","Seq_Id..NA.","Subtype","Location","Host","Collection_Date"))
+colnames(cleaned_metadata)<-c("S4","S6","Subtype","Location","Host","Date")
+
+H1N1_metadata<-subset(cleaned_metadata,Subtype=="H1N1")
+H3N2_metadata<-subset(cleaned_metadata,Subtype=="H3N2")
+B_metadata<-subset(cleaned_metadata,Subtype=="B")
+
+label<-c("strain","country","host","date")
+
+H1N1_S4<-subset(H1N1_metadata,select=-c(S6,Subtype))
+H1N1_S6<-subset(H1N1_metadata,select=-c(S4,Subtype))
+colnames(H1N1_S4)<-label
+colnames(H1N1_S6)<-label
+
+H3N2_S4<-subset(H3N2_metadata,select=-c(S6,Subtype))
+H3N2_S6<-subset(H3N2_metadata,select=-c(S4,Subtype))
+colnames(H3N2_S4)<-label
+colnames(H3N2_S6)<-label
+
+B_S4<-subset(B_metadata,select=-c(S6,Subtype))
+B_S6<-subset(B_metadata,select=-c(S4,Subtype))
+colnames(B_S4)<-label
+colnames(B_S6)<-label
+
+write.table(H1N1_S4,file=H1N1_S4_output,col.names = TRUE,row.names = FALSE,quote=FALSE,sep="\t")
+write.table(H1N1_S6,file=H1N1_S6_output,col.names = TRUE,row.names = FALSE,quote=FALSE,sep="\t")
+write.table(H3N2_S4,file=H3N2_S4_output,col.names = TRUE,row.names = FALSE,quote=FALSE,sep="\t")
+write.table(H3N2_S6,file=H3N2_S6_output,col.names = TRUE,row.names = FALSE,quote=FALSE,sep="\t")
+write.table(B_S4,file=B_S4_output,col.names = TRUE,row.names = FALSE,quote=FALSE,sep="\t")
+write.table(B_S6,file=B_S6_output,col.names = TRUE,row.names = FALSE,quote=FALSE,sep="\t")
