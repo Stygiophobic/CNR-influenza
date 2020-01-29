a609998d734aaa57e88275f881af0a608c901f1c
Ext-Hadrien REGUE01 <regueex@chu-lyon.fr@di3224su.chu-lyon.fr>
2020-01-29 16:01:06 +0100

    rajout des 4 sous types de virus

--- script/make_metadata.R
+++ script/make_metadata.R
@@ -1,58 +1,61 @@
 argv <- commandArgs(TRUE)
-
-#raw_metadata<-snakemake@input[[1]]
-#H1N1_S4_output<-snakemake@output[[1]]
-#H1N1_S6_output<-snakemake@output[[2]]
-#H3N2_S4_output<-snakemake@output[[3]]
-#H3N2_S4_output<-snakemake@output[[4]]
-#B_S4_output<-snakemake@output[[5]]
-#B_S4_output<-snakemake@output[[6]]
-#snakemake@config[["myparam"]]
-
 raw_metadata<-read.csv2(as.character(argv[1]))
-#H1N1_S4_output<-as.character(argv[2])
-#H1N1_S6_output<-as.character(argv[3])
-#H3N2_S4_output<-as.character(argv[4])
-#H3N2_S6_output<-as.character(argv[5])
-#B_S4_output<-as.character(argv[6])
-#B_S6_output<-as.character(argv[7])
+#subtype<-as.character(argv[2])
+#setwd("C:/Users/regueex/Desktop")
+#raw_metadata<-read.csv2("C:/Users/regueex/Desktop/metadata_raw.csv")
 
-#raw_metadata<-read.csv2("/home/hadrien/Bureau/CNR-influenza/temp_data/metadata_raw.csv")
+cleaned_metadata<-subset(raw_metadata,select=c("Seq_Id..HA.","Seq_Id..NA.",
+                                               "Subtype","Lineage","Location","Host",
+                                               "Collection_Date","Isolate_Id",
+                                               "Host_Age","Originating_Lab_Id"))
+colnames(cleaned_metadata)<-c("S4","S6","Subtype","Lineage","Location","Host","Date","ID_GISAID","Age","Laboratoire")
 
-cleaned_metadata<-subset(raw_metadata,select=c("Seq_Id..HA.","Seq_Id..NA.","Subtype","Location","Host","Collection_Date"))
-colnames(cleaned_metadata)<-c("S4","S6","Subtype","Location","Host","Date")
+cleaned_metadata$Subtype<-paste0(cleaned_metadata$Subtype,cleaned_metadata$Lineage)
 
-H1N1_metadata<-subset(cleaned_metadata,Subtype=="H1N1")
+cleaned_metadata<-cleaned_metadata[,-4]
+
+H1N1_metadata<-subset(cleaned_metadata,Subtype=="H1N1pdm")
 H3N2_metadata<-subset(cleaned_metadata,Subtype=="H3N2")
-B_metadata<-subset(cleaned_metadata,Subtype=="B")
+BVIC_metadata<-subset(cleaned_metadata,Subtype=="BVictoria")
+BYAM_metadata<-subset(cleaned_metadata,Subtype=="BYamagata")
 
-label<-c("strain","country","host","date")
+label<-c("strain","subtype","country","host","date","id_gisaid","age","laboratoire_id")
 
-H1N1_S4<-subset(H1N1_metadata,select=-c(S6,Subtype))
-H1N1_S6<-subset(H1N1_metadata,select=-c(S4,Subtype))
+H1N1_S4<-subset(H1N1_metadata,select=-c(S6))
+H1N1_S6<-subset(H1N1_metadata,select=-c(S4))
 colnames(H1N1_S4)<-label
 colnames(H1N1_S6)<-label
 
-H3N2_S4<-subset(H3N2_metadata,select=-c(S6,Subtype))
-H3N2_S6<-subset(H3N2_metadata,select=-c(S4,Subtype))
+H3N2_S4<-subset(H3N2_metadata,select=-c(S6))
+H3N2_S6<-subset(H3N2_metadata,select=-c(S4))
 colnames(H3N2_S4)<-label
 colnames(H3N2_S6)<-label
 
-B_S4<-subset(B_metadata,select=-c(S6,Subtype))
-B_S6<-subset(B_metadata,select=-c(S4,Subtype))
-colnames(B_S4)<-label
-colnames(B_S6)<-label
+BVIC_S4<-subset(BVIC_metadata,select=-c(S6))
+BVIC_S6<-subset(BVIC_metadata,select=-c(S4))
+colnames(BVIC_S4)<-label
+colnames(BVIC_S6)<-label
 
-#write.table(H1N1_S4,file=H1N1_S4,col.names = TRUE,row.names = FALSE,quote=FALSE,sep="\t")
-#write.table(H1N1_S6,file=H1N1_S6,col.names = TRUE,row.names = FALSE,quote=FALSE,sep="\t")
-#write.table(H3N2_S4,file=H3N2_S4,col.names = TRUE,row.names = FALSE,quote=FALSE,sep="\t")
-#write.table(H3N2_S6,file=H3N2_S6,col.names = TRUE,row.names = FALSE,quote=FALSE,sep="\t")
-#write.table(B_S4,file=B_S4,col.names = TRUE,row.names = FALSE,quote=FALSE,sep="\t")
-#write.table(B_S6,file=B_S6,col.names = TRUE,row.names = FALSE,quote=FALSE,sep="\t")
+BYAM_S4<-subset(BYAM_metadata,select=-c(S6))
+BYAM_S6<-subset(BYAM_metadata,select=-c(S4))
+colnames(BYAM_S4)<-label
+colnames(BYAM_S6)<-label
 
 write.table(H1N1_S4,file="temp_data/H1N1S4.tsv",col.names = TRUE,row.names = FALSE,quote=FALSE,sep="\t")
 write.table(H1N1_S6,file="temp_data/H1N1S6.tsv",col.names = TRUE,row.names = FALSE,quote=FALSE,sep="\t")
 write.table(H3N2_S4,file="temp_data/H3N2S4.tsv",col.names = TRUE,row.names = FALSE,quote=FALSE,sep="\t")
 write.table(H3N2_S6,file="temp_data/H3N2S6.tsv",col.names = TRUE,row.names = FALSE,quote=FALSE,sep="\t")
-write.table(B_S4,file="temp_data/BS4.tsv",col.names = TRUE,row.names = FALSE,quote=FALSE,sep="\t")
-write.table(B_S6,file="temp_data/BS6.tsv",col.names = TRUE,row.names = FALSE,quote=FALSE,sep="\t")
+write.table(BYAM_S4,file="temp_data/BYAMS4.tsv",col.names = TRUE,row.names = FALSE,quote=FALSE,sep="\t")
+write.table(BYAM_S6,file="temp_data/BYAMS6.tsv",col.names = TRUE,row.names = FALSE,quote=FALSE,sep="\t")
+write.table(BVIC_S4,file="temp_data/BVICS4.tsv",col.names = TRUE,row.names = FALSE,quote=FALSE,sep="\t")
+write.table(BVIC_S6,file="temp_data/BVICS6.tsv",col.names = TRUE,row.names = FALSE,quote=FALSE,sep="\t")
+
+
+#if (subtype=="H1N1S4") write.table(H1N1_S4,file=paste0(subtype,".tsv"),col.names = TRUE,row.names = FALSE,quote=FALSE,sep="\t")
+#if (subtype=="H1N1S6") write.table(H1N1_S6,file=paste0(subtype,".tsv"),col.names = TRUE,row.names = FALSE,quote=FALSE,sep="\t")
+#if (subtype=="H3N2S4") write.table(H3N2_S4,file=paste0(subtype,".tsv"),col.names = TRUE,row.names = FALSE,quote=FALSE,sep="\t")
+#if (subtype=="H3N2S6") write.table(H3N2_S6,file=paste0(subtype,".tsv"),col.names = TRUE,row.names = FALSE,quote=FALSE,sep="\t")
+#if (subtype=="BYAMS4") write.table(BYAM_S4,file=paste0(subtype,".tsv"),col.names = TRUE,row.names = FALSE,quote=FALSE,sep="\t")
+#if (subtype=="BYAMS6") write.table(BYAM_S6,file=paste0(subtype,".tsv"),col.names = TRUE,row.names = FALSE,quote=FALSE,sep="\t")
+#if (subtype=="BVICS4") write.table(BVIC_S4,file=paste0(subtype,".tsv"),col.names = TRUE,row.names = FALSE,quote=FALSE,sep="\t")
+#if (subtype=="BVICS6") write.table(BVIC_S6,file=paste0(subtype,".tsv"),col.names = TRUE,row.names = FALSE,quote=FALSE,sep="\t")
